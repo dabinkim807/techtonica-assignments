2201bf939e2374f179ca81cccb692b39d9e9ef7a
Dana <dabinkim807@gmail.com>
2023-02-17 08:26:29 -0500

    copied html from coin-flip proj

--- coin-flip-react/src/App.js
+++ coin-flip-react/src/App.js
@@ -3,9 +3,46 @@ import './App.css';
 function App() { 
   return (
     <div>
-      <header>
+      <h1>Coin Flip Game</h1>
+      <section class="row">
+        <section>
+          <h2>Result:</h2>
+          <div class="resultMessage"></div>
+          <div class="imgbox">
+            <img id="myImg" src="images/depositphotos_125165712-stock-illustration-businessman-hand-throwing-up-a.jpeg" alt="thumb flipping coin into air"/>
+          </div>
+        </section>
+        <section>
+          <h2>Your Guess:</h2>
+          <form id="form">
+            <div class="error"></div>
 
-      </header>
+            <div>
+              <input id="heads" name="coin" type="radio"></input>
+              <label for="heads" class="radio-left">Heads</label>
+      
+              <input id="tails" name="coin" type="radio"></input>
+              <label for="tails" class="radio-left">Tails</label>
+            </div>
+
+            <div>
+              <button type="submit" id="flip">Flip</button>
+              <button type="reset" id="try-again">Try Again</button>
+            </div>
+          </form>
+        </section>
+      </section>
+      <section class="scoreboard">
+        <h2>Scoreboard</h2>
+        <div class="scoreRow">
+          <div>
+            <span id="scoreWin">Wins: 0</span>
+          </div>
+          <div>
+            <span id="scoreLose">Losses: 0</span>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
