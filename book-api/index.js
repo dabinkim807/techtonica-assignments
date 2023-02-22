1c82890fd268557370769f06e7792377036b8085
Dana <dabinkim807@gmail.com>
2023-02-22 17:37:57 -0500

    fixed bugs and started server

--- book-api/index.js
+++ book-api/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const books = require('./books.js')
 
 const app = express();
 
 app.use(cors());
 
-const PORT = 5000;
+const PORT = 5001;
 
 app.listen(PORT, () => console.log(`Hello! Server is running on port ${PORT}`));
