3d9e4c022bac3e84576be1a4e2266a5c62bf3d29
Dana <dabinkim807@gmail.com>
2023-02-23 12:54:57 -0500

    created put route

--- book-api/index.js
+++ book-api/index.js
@@ -9,15 +9,18 @@ app.use(cors());
 // middleware that enables express to read JSON; when you make a request that uses Content-Type = application/json, this middleware parses that request
 app.use(express.json());
 
-// make API
+
+// ** GET requests **
+
+// returns homepage
 app.get('/', (req, res) => {
   res.json("Hello! This is my server!");
 })
-
+// returns all books in inventory
 app.get('/api/books', (req, res) => {
   res.json(books);
 })
-
+// search for a specific book
 app.get('/api/books/:bookID', cors(), async (req, res) => {
   // The request
   let requestedBook = req.params.bookID;
@@ -29,9 +32,13 @@ app.get('/api/books/:bookID', cors(), async (req, res) => {
       res.json(book);
     }
   }
+
+  // if there's no book with that ID,
+  res.send('no books');
 })
 
-// This responds a POST request for the homepage
+
+// ** POST request - create new book entry **
 app.post('/api/books', (req, res) => {
   let newBook = {
     isbn: req.body.isbn,
@@ -42,6 +49,21 @@ app.post('/api/books', (req, res) => {
   books.push(newBook);
 
   res.send("saved new book");
+})
+
+
+// ** PUT request - update existing book **
+app.put('/api/books/:bookID', (req, res) => {
+  let requestedBook = req.params.bookID;
+  for (let book of books) {
+    if (book.isbn === requestedBook) {
+      book.author = req.body.author;
+      book.title = req.body.title;
+      book.format = req.body.format;
+    }
+  }
+
+  res.send("updated book");
 })
 
 
