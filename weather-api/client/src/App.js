03155c1ec3cac06b397f25dd2eca08e524dc6de6
Dana <dabinkim807@gmail.com>
2023-03-01 19:54:08 -0500

    imported components

--- weather-api/client/src/App.js
+++ weather-api/client/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { useState } from "react";
+import Form from "./components/Form";
 
 
 function App() {
 
   return (
-    <div className="App">
-      This is my React!
+    <div>
+      <header>
+        <h1>Dana's OpenWeatherMap.org API</h1>
+      </header>
+      <Form />
     </div>
   );
 }
