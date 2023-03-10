70ec03e01cc89f3f15564cfee0da49504c609bf2
Dana <dabinkim807@gmail.com>
2023-03-09 19:56:29 -0500

    added final score text

--- trivia-api/client/src/components/score.js
+++ trivia-api/client/src/components/score.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 
-const Score = () => {
-
+const Score = (props) => {
+  // score={score} outOf={totalQuestions.length}
   return (
     <div className="score">
-      
+      You got {props.score}/{props.outOf} questions correct!
     </div>
   );
 }
